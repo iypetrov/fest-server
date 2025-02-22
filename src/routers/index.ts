06e519449ba48f15445f5565059a4c23b7205ba0
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-22 19:09:20 +0200

    drop users router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,13 +2,11 @@ import express from 'express';
 import { Router } from 'express';
 
 import auth from './auth';
-import users from './users';
 
 const router = express.Router();
 
 export default (): Router => {
     auth(router);
-    users(router);
     return router;
 }
 
