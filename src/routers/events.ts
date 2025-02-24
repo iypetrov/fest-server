7e7c6baa077c7ce330ad5cae96758dbe0ed7bf0f
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-24 16:54:28 +0200

    images dont require user auth

--- src/routers/events.ts
+++ src/routers/events.ts
@@ -15,7 +15,7 @@ export default (router: Router) => {
     router.post('/events', isAdmin, upload.single('file'), eventsController.create);
     router.get('/events', isClient, eventsController.getAll);
     router.get('/events/:id', isClient, eventsController.getById);
-    router.get('/events/:id/thumbnail', isClient, eventsController.getThumbnailById);
+    router.get('/events/:id/thumbnail', eventsController.getThumbnailById);
     router.put('/events/:id/thumbnail', isAdmin, upload.single('file'), eventsController.uploadThumbnail);
 };
 
